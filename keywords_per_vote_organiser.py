b6e510a887d89a484072e65e5945c3c6a6a9cea0
llmlks <lamelkas@gmail.com>
2019-10-01 17:03:34 +0300

    Fixes to grouping data by votes

--- keywords_per_vote_organiser.py
+++ keywords_per_vote_organiser.py
@@ -16,5 +16,39 @@ proposals_df = pd.read_json(PROPOSALS)
 votes_df = pd.read_csv(VOTE_DISTRIBUTIONS, ';').drop(columns=['Unnamed: 0', 'Unnamed: 0.1'])
 votes_df = votes_df[votes_df.AanestysValtiopaivaasia.isin(proposals_df.id.unique())]
 
-votes_df = votes_df.merge(proposals_df, left_on='AanestysValtiopaivaasia', right_on='id', how='left')
-votes_df.to_json('./data/votes_keywords_distributions.json', orient='records')
+unique_votes_df = votes_df.filter(
+  items=['AanestysId', 'IstuntoPvm', 'IstuntoVPVuosi', 'AanestysMitatoity', 'KohtaKasittelyVaihe', 'AanestysValtiopaivaasia'],
+).rename(columns={
+  'AanestysId': 'vote_id',
+  'IstuntoPvm': 'date',
+  'IstuntoVPVuosi': 'year',
+  'AanestysMitatoity': 'annulled',
+  'KohtaKasittelyVaihe': 'hearing_stage'
+}).drop_duplicates()
+
+def distribution_from_row(vote):
+  return({
+    'group': vote.Ryhma,
+    'type': vote.Tyyppi,
+    'vote_counts': {
+      'for': int(vote.Jaa),
+      'against': int(vote.Ei),
+      'empty': int(vote.Tyhjia),
+      'away': int(vote.Poissa)
+    }
+  })
+
+def combine_distribution_to_vote(vote):
+  distribution_rows = votes_df[votes_df.AanestysId == vote]
+  distributions = {
+    'distr': []
+  }
+
+  distribution_rows.apply(lambda x: distributions['distr'].append(distribution_from_row(x)), axis=1)
+
+  return(str(distributions))
+
+unique_votes_df = unique_votes_df.merge(proposals_df, left_on='AanestysValtiopaivaasia', right_on='id', how='left').assign(distribution=lambda x: x.vote_id)
+unique_votes_df.distribution = unique_votes_df.distribution.apply(lambda x: combine_distribution_to_vote(x))
+
+unique_votes_df.to_json('./data/votes_keywords_distributions.json', orient='records')
